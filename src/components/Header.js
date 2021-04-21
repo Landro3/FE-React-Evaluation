219a0d63158d76a491e58565a783fafc625b007f
Andrew Landis <andrew.landis3@gmail.com>
2021-04-21 19:53:04 -0400

    Header styling added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import userLogo from '../images/person-fill.svg';
+import brandLogo from '../images/NOINC_Logo.svg';
+import '../styles/Header.css';
 
 export default function Header() {
 
   // Will update No Inc and person icon
   return (
-    <Navbar>
+    <Navbar expand="lg" variant="dark" style={{padding: 0}}>
       <Navbar.Brand>
-        <Link to="/Home">
-          No Inc.
-        </Link>
+        <img src={brandLogo} alt="No Inc. Logo" />
       </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse className="justify-content-end">
         <Nav>
+          <div className="v-sep" />
           <Link to="/Home">Home</Link>
+          <div className="v-sep" />
           <Link to="/Interests">Interests</Link>
+          <div className="v-sep" />
           <Link to="/Skills">Skills</Link>
         </Nav>
+        <div className="v-sep" />
         <Navbar.Text>
-          `icon` username
+          <img src={userLogo} alt="Username Logo" />
+          <span>username</span>
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
