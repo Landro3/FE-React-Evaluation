08988b8e2f7e4279ce0fc1a3fdd64efb1e44695b
Andrew Landis <andrew.landis3@gmail.com>
2021-04-22 15:25:53 -0400

    header current page styling and login username

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import userLogo from '../images/person-fill.svg';
 import brandLogo from '../images/NOINC_Logo.svg';
 import '../styles/Header.css';
 
-export default function Header() {
+export default function Header(props) {
+  const username = useSelector(state => state.user.userName);
 
   // Will update No Inc and person icon
   return (
     <Navbar expand="lg" variant="dark" style={{padding: 0}}>
       <Navbar.Brand>
-        <img src={brandLogo} alt="No Inc. Logo" />
+        <Link to="/Home">
+          <img src={brandLogo} alt="No Inc. Logo" />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse className="justify-content-end">
         <Nav>
-          <div className="v-sep" />
-          <Link to="/Home">Home</Link>
-          <div className="v-sep" />
-          <Link to="/Interests">Interests</Link>
-          <div className="v-sep" />
-          <Link to="/Skills">Skills</Link>
+          <NavLink className="navLink" activeClassName="active" to="/Home"><div className="homeLink">Home</div></NavLink>
+          <NavLink className="navLink" activeClassName="active" to="/Interests"><div className="interestsLink">Interests</div></NavLink>
+          <NavLink className="navLink" activeClassName="active" to="/Skills"><div className="skillsLink">Skills</div></NavLink>
         </Nav>
         <div className="v-sep" />
         <Navbar.Text>
           <img src={userLogo} alt="Username Logo" />
-          <span>username</span>
+          <span>{username}</span>
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
